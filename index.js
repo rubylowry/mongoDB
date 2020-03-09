093c702ab0ed676260f6eeb3f143574a202807d8
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2020-03-09 01:39:58 +0000

    connecting to backend

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const bcryptjs = require('bcryptjs');
-const config = require('./config.json');
-const product = require('./products.json') // external json data from mockaroo
+const bodyParser = require('body-parser'); //to parse all data coming from the user and db
+const cors = require('cors'); //to include cross orgin request
+const bcryptjs = require('bcryptjs');//to hash and compare password in an encrypted method
+const config = require('./config.json');//has credentials
+const product = require('./Products.json');//external json data from mockaroo api
 const Product = require('./models/products.js');
 const User = require('./models/users.js');
 
+
 const port = 3000;
+
 //connect to db
-// const mongodbURI = 'mongodb+srv://rubylowry:okiDOKES77@rubydatabase-lrehb.mongodb.net/test?retryWrites=true&w=majority';
-const mongodbURI = `mongodb+srv://${config.MONGO_USER}:${config.MONGO_PASSWORD}@${config.MONGO_CLUSTER_NAME}.mongodb.net/test?retryWrites=true&w=majority`;
+// const mongodbURI = 'mongodb+srv://beulasamuel:<password>@beudb-sxyu7.mongodb.net/test?retryWrites=true&w=majority';
+const mongodbURI = `mongodb+srv://${config.MONGO_USER}:${config.MONGO_PASSWORD}@${config.MONGO_CLUSTER_NAME}.mongodb.net/shop?retryWrites=true&w=majority`;
 mongoose.connect(mongodbURI, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(()=> console.log('DB connected!'))
 .catch(err =>{
   console.log(`DBConnectionError: ${err.message}`);
 });
+
 //test the connectivity
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -25,12 +28,12 @@ db.once('open', function() {
   console.log('We are connected to mongo db');
 });
 
-app.use((req, res, next)=>{
+app.use((req,res,next)=>{
   console.log(`${req.method} request for ${req.url}`);
   next();//include this to go to the next middleware
 });
 
-
+//including body-parser, cors, bcryptjs
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
@@ -38,14 +41,12 @@ app.use(cors());
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-// if the user requests for all products this show everything in file
-
-app.get('/allProducts', (req,res)=>{ // this is for read
+app.get('/allProducts', (req,res)=>{
   res.json(product);
 
 });
 
-app.get('/products/p=:id', (req,res)=>{ //this is for read
+app.get('/products/p=:id', (req,res)=>{
   const idParam = req.params.id;
 
   for (let i = 0; i < product.length; i++){
@@ -57,77 +58,42 @@ app.get('/products/p=:id', (req,res)=>{ //this is for read
 
 });
 
+//Add products.
+app.post('/addProduct', (req,res)=>{
+  //checking if user is found in the db already
+  Product.findOne({name:req.body.name},(err,productResult)=>{
 
-//adding product
-app.post('/addProduct', (req,res)=>{ // this is for create
-
-  Product.findOne({productName:req.body.productName},(err,productResult)=>{
     if (productResult){
-      res.send('We already have this product. Please try another one');
-    } else {
-       const product = new Product({
-         _id : new mongoose.Types.ObjectId,
-         productName : req.body.productName,
-         price : req.body.price,
-       });
-
-       product.save().then(result =>{
-         res.send(result);
-       }).catch(err => res.send(err));
-    }
-
-  })
-
-
-});
-
-//register user
-app.post('/registerUser', (req,res)=>{ // this is for create
-
-  User.findOne({username:req.body.username},(err,userResult)=>{
-    if (userResult){
-      res.send('Username taken already. Please try another one');
+      res.send('product added already');
     } else{
-      const hash = bcryptjs.hashSync(req.body.password);
-       const user = new User({
-         _id : new mongoose.Types.ObjectId,
-         username : req.body.username,
-         email : req.body.email,
-         password :hash
-       });
 
-       user.save().then(result =>{
+       const dbProduct = new Product({
+         _id : new mongoose.Types.ObjectId,
+         name : req.body.name,
+         price : req.body.price,
+         imageUrl : req.body.imageUrl
+       });
+       //save to database and notify the user accordingly
+       dbProduct.save().then(result =>{
          res.send(result);
        }).catch(err => res.send(err));
     }
 
   })
 
-
-});
-
-
-// hash sync
-// compare sync
-//get all user
-app.get('/allUsers', (req,res)=>{ // this is for read
-  User.find().then(result =>{
-    res.send(result);
-  })
 
 });
 
 //get all products
-app.get('/allProductsFromDB', (req,res)=>{ // this is for read
+app.get('/allProductsFromDB', (req,res)=>{
   Product.find().then(result =>{
     res.send(result);
   })
 
 });
 
-
 //delete a product
-app.delete('/deleteProduct/:id',(req,res)=>{ // deletes
+app.delete('/deleteProduct/:id',(req,res)=>{
   const idParam = req.params.id;
   Product.findOne({_id:idParam}, (err,product)=>{ //_id refers to mongodb
     if (product){
@@ -140,7 +106,6 @@ app.delete('/deleteProduct/:id',(req,res)=>{ // deletes
   }).catch(err => res.send(err));
 });
 
-// to ulter a product
 app.patch('/updateProduct/:id',(req,res)=>{
   const idParam = req.params.id;
   Product.findById(idParam,(err,product)=>{
@@ -158,22 +123,58 @@ app.patch('/updateProduct/:id',(req,res)=>{
 });
 
 
+//register user
+app.post('/registerUser', (req,res)=>{ // this is for create
+  //checking if user is found in the db already
+  User.findOne({username:req.body.username},(err,userResult)=>{
+
+    if (userResult){
+      res.send('username taken already. Please try another one');
+    } else{
+       const hash = bcryptjs.hashSync(req.body.password); //hash the password
+       const user = new User({
+         _id : new mongoose.Types.ObjectId,
+         username : req.body.username,
+         email : req.body.email,
+         password :hash
+       });
+       //save to database and notify the user accordingly
+       user.save().then(result =>{
+         res.send(result);
+       }).catch(err => res.send(err));
+    }
+
+  })
+
+
+});
+
+//get all user
+app.get('/allUsers', (req,res)=>{
+  User.find().then(result =>{
+    res.send(result);
+  })
+
+});
+
 //login the user
-app.post('/loginUser', (req,res)=>{ // this is for create
+app.post('/loginUser', (req,res)=>{
   User.findOne({username:req.body.username},(err,userResult)=>{
     if (userResult){
       if (bcryptjs.compareSync(req.body.password, userResult.password)){
         res.send(userResult);
       } else {
-        res.send('Not authorized.');
+        res.send('not authorized');
       }//inner if
     } else {
-       res.send('User was not found. Please register');
+       res.send('user not found. Please register');
     }//outer if
   });//findOne
 });//post
 
 
-// keep this at the bottom of code so you can always see any errors
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+//keep this always at the bottom so that you can see the errors reported
+app.listen(port, () => console.log(`Mongodb app listening on port ${port}!`))
